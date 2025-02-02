2b8475c618c0f75e46114cb009442d1af68a8a9b
Jason Jones <jasonm_jones@yahoo.com>
2025-02-01 17:34:19 -0700

    Initial Commit

---
+++ Grid Carbon Intensity/Grid_Carbon_IntensityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_Carbon_IntensityApp.swift
+//  Grid Carbon Intensity
+//
+//  Created by Jason Jones on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Grid_Carbon_IntensityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Grid Carbon IntensityTests/Grid_Carbon_IntensityTests.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_Carbon_IntensityTests.swift
+//  Grid Carbon IntensityTests
+//
+//  Created by Jason Jones on 2/1/25.
+//
+
+import Testing
+@testable import Grid_Carbon_Intensity
+
+struct Grid_Carbon_IntensityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
